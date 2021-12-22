2d4b575ddd808bce4462911dd8c591cdb20d4bb8
davidvictoria <davidvm99@gmail.com>
2021-12-22 17:38:12 +0100

    Added return to controllerç

--- app/CoreContext/Users/Application/Commands/UserBuyHandler.php
+++ app/CoreContext/Users/Application/Commands/UserBuyHandler.php
@@ -13,18 +13,18 @@ class UserBuyHandler
         $this->userRepository = $userRepository;
     }
 
-
     public function handle(UserBuy $userBuy){
 
         $user = $this->userRepository->byId($userBuy->user()->id);
         $wallet = $this->userRepository->findUserWallet($userBuy->user()->id, $userBuy->symbol());
 
+        //Deuda tecnica cambiar a Excepcion en otro archivo.
         if ($user->money < $userBuy->cost())
-            throw new \Exception("User does not have enought money.");
+            throw new \Exception("User does not have enough money.");
 
         $user->money -= $userBuy->cost();
-
         $amount = $wallet->wallet + ($userBuy->cost() / $userBuy->price());
+
         $this->userRepository->addToWallet($user->id, $userBuy->symbol(), $amount);
         $this->userRepository->save($user);
     }
