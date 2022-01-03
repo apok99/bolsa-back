96073a53e8415732edeb796be79f9c22c883a99c
david <david>
2022-01-03 09:33:03 -0500

    hola

---
+++ app/CoreContext/Season/Applicaction/Command/GetRandomGiftCommandHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\CoreContext\Season\Applicaction\Command;
+
+use App\CoreContext\Companies\Application\Querys\FindPriceBySymbol;
+use App\CoreContext\Companies\Application\Querys\FindPriceBySymbolHandler;
+
+class GetRandomGiftCommandHandler
+{
+    public function handle(GetRandomGiftCommand $getRandomGiftCommand)
+    {
+
+    }
+}
