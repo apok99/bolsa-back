811bbb76f486a29c64c145b2e92ae8ca7ee9fbe5
e-aleixandre <e.montana@sesametime.com>
2022-02-26 17:56:43 +0100

    Adds mailhog as a docker container, adds symfony/mailer and basic configuration

---
+++ symfony/README.md
@@ -0,0 +1,3 @@
+# Instrucciones
+
+Aquí iré añadiendo instrucciones para montar el proyecto en local, ya que puede requerir alguna configuración extra.
--- symfony/src/Company/Infrastructure/Controller/v1/GetAllCompaniesController.php
+++ symfony/src/Company/Infrastructure/Controller/v1/GetAllCompaniesController.php
@@ -8,13 +8,22 @@ use App\Api\Domain\ValueObject\ApiResponse;
 use App\Security\Domain\Service\AuthSessionServiceInterface;
 use App\Shared\Infrastructure\Controller\v1\BaseController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GetAllCompaniesController extends BaseController
 {
     #[Route('/companies', methods: ['GET'])]
-    public function __invoke(AuthSessionServiceInterface $authSessionService): JsonResponse
+    public function __invoke(
+        AuthSessionServiceInterface $authSessionService,
+        MailerInterface $mailer
+    ): JsonResponse
     {
+        $mail = new Email();
+        $mail->to('test@ateasd.com')->from('from@from.com')->subject('Subject')->text('Contents');
+        $mailer->send($mail);
+
         $user = $authSessionService->user();
 
         $response = new ApiResponse([
